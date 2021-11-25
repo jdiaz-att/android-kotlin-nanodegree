b63136b6c8fc73a6dcd11cd7974165f4bdf858ff
Julio Diaz <jd594n@att.com>
2021-11-25 01:13:34 -0800

    Add c2-starter app

---
+++ c2-starter/app/src/main/java/com/udacity/asteroidradar/main/AsteroidAdapter.kt
@@ -0,0 +1,40 @@
+package com.udacity.asteroidradar.main
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.udacity.asteroidradar.Asteroid
+
+class AsteroidAdapter : ListAdapter<DataItem, RecyclerView.ViewHolder>(AsteroidDiffCallback()) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
+
+class AsteroidDiffCallback : DiffUtil.ItemCallback<DataItem>() {
+    override fun areItemsTheSame(oldItem: DataItem, newItem: DataItem): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: DataItem, newItem: DataItem): Boolean {
+        return oldItem == newItem
+    }
+
+}
+
+sealed class DataItem {
+    data class AsteroidItem(val asteroid: Asteroid): DataItem() {
+        override val id = asteroid.id
+    }
+
+    object Header: DataItem() {
+        override val id = Long.MIN_VALUE
+    }
+
+    abstract val id: Long
+}
